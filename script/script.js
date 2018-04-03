6fd6040ec764c67a8651a0b6f171a180f19d14d2
sara <saraa379@yahoo.com>
2018-04-03 19:59:22 +0200

    equal added

--- script/script.js
+++ script/script.js
@@ -10,6 +10,7 @@
     messagingSenderId: "902565878040"
   };
   firebase.initializeApp(config);
+  const db = firebase.database();
 
 
 
@@ -23,25 +24,67 @@ window.addEventListener('load', () => {
 			lastOperator: "",
 			lastNumber: "",
 			result: 0,
+			resultFinal: 0,
 			inputNr: "",
-			tempCalc: ""
+			tempCalc: "",
+			counter: 0
 		}, // data
 
 		methods: {
 			saveNumber: function(event) {
 				let nr = Number(this.inputNr);
-				this.lastInput = nr;
+				this.lastInput = "number";
 				this.lastNumber = nr;
+				this.counter = this.counter + 1;
+				this.result = nr;
 
 				if (this.lastOperator == "") {
 					this.tempCalc = nr;
-					this.result = nr;
+					this.resultFinal = nr;
+					this.lastNumber = nr;
+				} else if (this.lastOperator == "add") {
+					this.tempCalc += " " + nr;
 				}
 
 				this.inputNr = "";
 			},
-			hide: function(event) {
-				this.displayStatus = false;
+			clear: function(event) {
+				this.lastInput = "";
+				this.lastOperator = "";
+				this.result = 0;
+				this.resultFinal = 0;
+				this.inputNr = "";
+				this.tempCalc = "";
+				this.counter = 0;
+				this.lastNumber = "";
+			},
+			add: function(event) {
+				this.lastOperator = "add";
+				this.tempCalc += " +";
+				this.lastInput = "operator";
+				this.counter = this.counter + 1;
+				if (this.counter > 3) {
+					this.resultFinal = this.resultFinal + this.lastNumber;
+					this.result = this.resultFinal;
+				}
+			},
+			calcFinal: function(event) {
+				switch (this.lastOperator) {
+					case "add":
+						this.resultFinal = this.resultFinal + this.lastNumber;
+						this.tempCalc += " = " + this.resultFinal; 
+					    break;
+					case "minus":
+					    console.log("minus");
+					    break;
+					} //switch
+				//Add calculation into db
+				if (this.tempCalc != "") {
+				      db.ref('calc/').push(this.tempCalc);
+		    	} //end of if
+
+				//clear the field
+				this.clear();
 			}
 		}  // methods
 	});  // Vue
